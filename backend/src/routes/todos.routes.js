083b23f83d74cb35625dbf9a56b75a56dbe7119e
MarceloOrtegaDev <ortegamarcelodavid77@gmail.com>
2024-09-16 12:32:03 -0300

    Se completó el trabajo práctico

--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import { borrarTodos, editarTarea, getAllTodosCtrl, postTodos, tareasXid } from "../controllers/todos.controllers.js";
 import validarJwt from "../middlewares/validar-jwt.js";
 
 const todosRouter = Router();
 
 todosRouter.get("/", validarJwt, getAllTodosCtrl);
+todosRouter.get("/:id", validarJwt, tareasXid)
+todosRouter.delete("/:id", validarJwt, borrarTodos)
+todosRouter.put("/:id", editarTarea)
+todosRouter.post("/", validarJwt, postTodos);
 
 export { todosRouter };
